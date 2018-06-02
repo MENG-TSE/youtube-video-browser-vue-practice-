8b0d905a813360dba51b706be5d6a1532aa644bd
MENG-TSE <jimmylin0179@gmail.com>
2018-06-02 18:24:12 +0800

    yotube api finish

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+const API_KEY ='AIzaSyBjSqpQChtV1o_zU5vKgRPCsXixS1KA1ik' ;
+// 'AIzaSyCx95SsPV4GKc25aVF7Ix_aCcgDPGaZQ7M'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        SearchBar : SearchBar
+    },
+    methods: {
+
+        onTermChange: function(searchTerm){
+            axios.get('https://www.googleapis.com/youtube/v3/search',{
+                params:{
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchTerm      ///q = query
+                }
+            })
+            .then(response => console.log(response));
+        }
+    }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
+
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <input @input="onInput">
+    </div>
+</template>
+
+
+
+
+
+
+<script>
+export default {
+    naem: 'SearchBar',
+    methods: {
+        onInput: function(event){
+            this.$emit('termChange', event.target.value);
+        }
+
+    }
+}
+</script>
+
+
+
+
+
+
+<style>
+
+</style>
